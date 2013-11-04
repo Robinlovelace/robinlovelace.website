6b57534360b4e09f7abc4dce7f94ba0b7a0bbe57
Robin Lovelace <rob00x@gmail.com>
2013-11-04 03:30:01 +0000

    mod

--- javascripts/quotes.js
+++ javascripts/quotes.js
@@ -1,4 +1,4 @@
-var d = new Date()%/10
+var d = new Date()
 var quote=new Array("Ownership of cars, once considered a rite of passage into the adult world of property relationships, has increasingly lost ground to leasing arrangements. (Rifkin, 2001, p. 220).",
 "[Poor people] travel less often and over shorter distances than the average population, but the social and economic consequances of this behaviour, such as reduced work and educational opportunities and poor access to healthcare and other essential services, all to often have been overlooked. In part this is because the policy professionals have poorly understood `transport poverty' and its impact on social welfare (Lucas, 2004)",
 "Simplicity is the hallmark of any good theory and apparent complexity is often simplicity in disguise. (Batty 1976) Urban Modelling.",
@@ -8,7 +8,7 @@ var quote=new Array("Ownership of cars, once considered a rite of passage into t
 "Future ages will certainly look back upon us as a people so immersed in the pursuit of wealth as to be blind to high considerations. They will charge us with having culpably allowed the destruction of some of those records of Creation which we had it in our power to preserve; and while professing to regard every living thing as the direct handiwork and best evidence of a Creator, yet, with a strange inconsistency, seeing many of them perish irrecoverably from the face of the earth, uncared for and unkown. Alfred Russel Wallace, 1863.",
 "Mass production of liquid biofuels fermented from annual harvests of crop or residual biomass would require large storages of either cereal or cellulosic feed stocks or the produced ethanol (or both), and the bulkiness of residues and relatively low energy densities of all of these materials would make such storages more costly than those of refined oils. (Smil 2010)")
 //document.write(d.getDate())
-document.write(quote[d.getDate()] + " ")
+document.write(quote[d.getDate() %/10] + " ")
 //document.write(weekday[d.getDay()] + " ")
 //document.write(d.getDate() + ". ")
 //document.write(monthname[d.getMonth()] + " ")
