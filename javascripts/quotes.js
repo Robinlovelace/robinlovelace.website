7032eca9b4a5da5b4c4bc0a4a8968c4c950c54c8
Robin Lovelace <rob00x@gmail.com>
2013-11-04 02:03:33 +0000

    quotes

---
+++ javascripts/quotes.js
@@ -0,0 +1,19 @@
+<script type="text/javascript">
+var d = new Date()
+var quote=new Array("Jan",
+"Feb",
+"Mar",
+"Apr",
+"May",
+"Jun",
+"Jul",
+"Aug",
+"Sep",
+"Oct",
+"Nov",
+"Dec")
+//document.write(weekday[d.getDay()] + " ")
+//document.write(d.getDate() + ". ")
+//document.write(monthname[d.getMonth()] + " ")
+//document.write(d.getFullYear())
+</script>
