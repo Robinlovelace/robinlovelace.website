df94bfd9e45e87de3ca425969a7fdcfae7ac0632
Robin Lovelace <rob00x@gmail.com>
2013-11-04 02:20:45 +0000

    lucas

--- javascripts/quotes.js
+++ javascripts/quotes.js
@@ -1,8 +1,9 @@
 var d = new Date()
 var quote=new Array("Jan",
 "Feb",
-"Mar",
-"Apr",
+"Ownership of cars, once considered a rite of passage into the adult world of property relationships, has increasingly lost ground to leasing arrangements.
+(Rifkin, 2001, p. 220).",
+"[Poor people] travel less often and over shorter distances than the average population, but the social and economic consequances of this behaviour, such as reduced work and educational opportunities and poor access to healthcare and other essential services, all to often have been overlooked. In part this is because the policy professionals have poorly understood `transport poverty' and its impact on social welfare (Lucas, 2004)",
 "May",
 "Jun",
 "Jul",
@@ -11,8 +12,8 @@ var quote=new Array("Jan",
 "Oct",
 "Nov",
 "Dec")
-document.write(d.getDay())
-document.write(quote[d.getDay()] + " ")
+// document.write(d.getDate())
+document.write(quote[d.getDate()] + " ")
 //document.write(weekday[d.getDay()] + " ")
 //document.write(d.getDate() + ". ")
 //document.write(monthname[d.getMonth()] + " ")
